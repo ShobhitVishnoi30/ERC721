992f0dfcd31be4dfdb2035dab4c3156778e7c51c
shobhitvishnoi30 <vishnoishobhit201@gmail.com>
2022-09-18 11:40:24 +0530

    feat:added ERC721

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# ERC721
-A standard ERC721 contract
+## Install and Setup
+
+### Clone
+
+```
+git clone https://github.com/ShobhitVishnoi30/ERC721.git
+
+```
+### Installation
+
+Install project's dependencies:
+
+```
+cd ERC721
+
+npm install
+
+```
+### Set up .env
+
+create a new .env file by copying it's content from env.example and filling in your secrets
+
+```
+cp .env.example .env
+
+```
+
+## Building the projects
+
+
+### compile
+
+Compile the contracts:
+
+```
+npm run compile
+
+```
+
+### Clean
+
+Delete the smart contract artifacts, the coverage reports and the Hardhat cache:
+
+```
+npm run clean
+
+```
+
+### Testing
+
+Run the tests:
+
+```
+npm run test
+
+```
+
+
+### Deployment
+
+Deploy the contracts:
+
+```
+npm run deploy
+
+```
---
+++ scripts/deploy.js
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const TestERC721 = await hre.ethers.getContractFactory("TestERC721");
+  const testERC721 = await TestERC20.deploy("testERC721", "TERC");
+
+  await testERC721.deployed();
+
+  console.log("Test ERC20 deployed to:", testERC721.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
